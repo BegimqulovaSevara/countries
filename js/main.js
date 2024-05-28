258f708b1d724b84478eba9bab1dad989ab76531
BegimqulovaSevara <sevarabegimkulova58@gmail.com>
2024-05-28 23:07:44 +0500

    Add header and dark mode

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// const elDarkModeButton = document.querySelector('.dark-button');
+
+// elDarkModeButton.addEventListener('click', function() {
+//     document.body.classList.add('dark-mode');
+// });
+// elDarkModeButton.addEventListener('double click', function() {
+//     document.body.classList.remove('dark-mode');
+// });
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
