3e7feafb502d5a0495f3c1d4f86567bfb5ad29c5
BegimqulovaSevara <sevarabegimkulova58@gmail.com>
2024-06-01 23:36:19 +0500

    Toggle dark-mode corrected images

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
-// const elDarkModeButton = document.querySelector('.dark-button');
+const elDarkModeButton = document.querySelector('.dark-button');
 
-// elDarkModeButton.addEventListener('click', function() {
-//     document.body.classList.add('dark-mode');
-// });
-// elDarkModeButton.addEventListener('double click', function() {
-//     document.body.classList.remove('dark-mode');
-// });
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+elDarkModeButton.addEventListener( 'click', function() {
+  document.body.classList.toggle('dark-mode');
+});
