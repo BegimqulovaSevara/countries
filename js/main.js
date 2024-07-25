9a50019f7a66e7c4319a2790ef0be6eab4a8cf62
BegimqulovaSevara <sevarabegimkulova58@gmail.com>
2024-07-25 21:19:01 +0500

    Save darkmode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 const elDarkModeButton = document.querySelector('.dark-button');
 
-elDarkModeButton.addEventListener( 'click', function() {
-  document.body.classList.toggle('dark-mode');
-});
+if(localStorage.getItem('darkmode') === document.body.classList.toggle('dark-mode')){
+  document.body.classList = 'dark-mode'
+}else{
+}
+
+
+if(elDarkModeButton){
+  elDarkModeButton.addEventListener( 'click', function(evt) {
+    evt.preventDefault()
+    localStorage.setItem('darkmode', document.body.classList.toggle('dark-mode'))
+  });
+}
+
+
+// elDarkModeButton.addEventListener( 'click', function() {
+//   document.body.classList.toggle('dark-mode');
+// });
